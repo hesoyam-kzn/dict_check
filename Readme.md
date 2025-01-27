38d9393ac84bcf1277d88089582fc5ba18307890
hesoyam-kzn <dimaswat@gmail.com>
2025-01-27 19:24:51 +0100

    First dictionary iteration

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Simple script for new words learning.
+
+How to use:
+1. Add .csv with words in format %word%,%translation% in each row (example test.csv is already in repo)
+2. Provide path to this .csv file into doc_path parameter of dict_main.py
+3. Start dict_main.py
+4. Enjoy learning new words!
---
+++ dict_main.py
@@ -0,0 +1,83 @@
+from random import choice, randint
+import os
+import string
+
+
+# reading rows from the passed .csv and saving them into the list of 2-element tuples
+def read_raw_doc(d_path):
+    lines = []
+    lis = []
+    with open(doc_path, 'r', encoding='utf8') as file:
+        for f in file:
+            lines.append(f.rstrip("\n"))
+
+    for line in lines:
+        m, k = line.split(',')
+        lis.append({"word": m, "tr": k})
+
+    file.close()
+    return lis
+
+
+# adding guessed word to the csv, where guessed words listed
+def count_guessed(word, mean, guessed_path):
+    pass
+
+
+def choose_four(seq):
+    res = []
+    while len(res) < 4:
+        tmp_val = choice(seq)
+        if tmp_val not in res:
+            res.append(tmp_val)
+    return res
+
+
+# method shows you once a word and several options for answer, you need to choose a correct transation
+def try_guess(c_set):
+    word = c_set[randint(0, 3)]
+    print(f"Guess word {word['word'].upper()} \n")
+    opts = []
+    for i in range(len(c_set)):
+        opts.append(f'{i + 1}. {c_set[i]["tr"]}')
+    print(*opts, sep='\n')
+
+    return word, opts
+
+
+# ------------- program parameters ---------------#
+# path to origin .csv with word-meaning pairs
+doc_path = f"{os.getcwd()}\\test.csv"
+# path to the .csv, which we'll fill with the pair of word-meaning + counter incrementation
+guessed_path = ''
+# ------------- parameters end ---------------#
+
+pairs = read_raw_doc(doc_path)
+
+while True:
+    cur_set = choose_four(pairs)
+    w, op = try_guess(cur_set)
+
+    correctly = False
+    while not correctly:
+        us_in = input(f'\nHow to translate? \n')
+        if len(us_in) > 0 and us_in.isdigit():
+            variant = int(us_in)
+            if us_in in '1234':
+                a = w['tr']
+                b = op[variant - 1][3:]
+                if a == b:
+                    print('\nYou god damned right.\n')
+                    correctly = True
+                else:
+                    print('\nYou lost, filthy liar!\n')
+                    correctly = True
+            else:
+                print("Try once more time, wrong character. Use 1-4 digits.")
+                continue
+        else:
+            print("Try once more time, empty \\ wrong character. Use 1-4 digits.")
+            continue
+
+
+
