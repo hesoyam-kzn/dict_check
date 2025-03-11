b06c2c7ff0dda22983b07e3f58de0ff0c6b8a0ba
hesoyam-kzn <dimaswat@gmail.com>
2025-03-11 19:17:10 +0100

    Added dummy progress logger into the separate file.

--- dict_main.py
+++ dict_main.py
@@ -1,7 +1,15 @@
 from random import choice, randint, shuffle
 import os
-import string
+from os import path, remove
 
+
+# ------------- program parameters ---------------#
+# path to origin .csv with word-meaning pairs
+doc_path = f"{os.getcwd()}\\test.csv"
+# path to the .csv, which we'll fill with the pair of word-meaning + counter incrementation
+guessed_path = ''
+log_name = "logfile.txt"
+# ------------- parameters end ---------------#
 
 # reading rows from the passed .csv and saving them into the list of 2-element tuples
 def read_raw_doc(d_path):
@@ -19,9 +27,24 @@ def read_raw_doc(d_path):
     return lis
 
 
-# adding guessed word to the csv, where guessed words listed
-def count_guessed(word, mean, guessed_path):
-    pass
+# Erasing existing progress data file (TODO: implement better, now is not working as expected)
+def clear_progress(args):
+    for i in range(len(args)):
+        if args[i] == "-mode":
+            if args[i+1].lower() == "c":
+                if path.exists(log_name):
+                    remove(log_name)
+            else:
+                print("Continuing with existing data about learning\n")
+
+
+def log_file_work(file):
+    if path.exists(file):
+        return open(file, "a", encoding="utf-8")
+    else:
+        return open(file, "w+", encoding="utf-8")
+
+
 
 
 def choose_four(seq):
@@ -33,7 +56,7 @@ def choose_four(seq):
     return res
 
 
-# method shows you once a word and several options for answer, you need to choose a correct transation
+# method shows you one a word and several options for answer, you need to choose a correct transation
 def try_guess(c_set):
     word = c_set[randint(0, 3)]
     shuffle(c_set)
@@ -46,18 +69,14 @@ def try_guess(c_set):
     return word, opts
 
 
-# ------------- program parameters ---------------#
-# path to origin .csv with word-meaning pairs
-doc_path = f"{os.getcwd()}\\test.csv"
-# path to the .csv, which we'll fill with the pair of word-meaning + counter incrementation
-guessed_path = ''
-# ------------- parameters end ---------------#
 
+# Main block of the program #
 pairs = read_raw_doc(doc_path)
 guessed_per_session = dict()
 sums = 0
+log = log_file_work(log_name)
 
-while sums < 15:
+while sums < 5:
     cur_set = choose_four(pairs)
     w, op = try_guess(cur_set)
 
@@ -71,7 +90,7 @@ while sums < 15:
                 a = w['tr']
                 b = op[variant - 1][3:]
                 if a == b:
-                    print('\n\033[32mYou god damned right.\033[0m\n')
+                    print('\n\033[32mYou god damn right.\033[0m\n')
                     correctly = True
                     guessed_per_session[tmp_tup] = guessed_per_session.setdefault(tmp_tup, 0) + 1
                     sums += 1
@@ -94,3 +113,7 @@ else:
     for g, u in guessed_per_session.items():
         print(f'{g[0]} ({g[1]}) +{u}')
 
+# Adding files to logfile.txt for measuring progress and filtering
+for k,v in guessed_per_session.items():
+    log.write(f'{k[0]},{k[1]}:{v}\n')
+log.close()
